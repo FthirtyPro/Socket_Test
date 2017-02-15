a3f590c17b95458acc1d4380087cdcade35c106b
fthirtypro@163.com <lijing000000>
2017-02-15 12:36:41 +0800

    one

---
+++ Assets/ChatManger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class ChatManger : MonoBehaviour {
+
+
+    public string IPstring;
+    private int Ip;
+
+    private Socket SocketClient;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void ClinetConect()
+    {
+        SocketClient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        SocketClient.Connect(new IPEndPoint(IPAddress.Parse(IPstring), Ip));
+    }
+
+
+}
